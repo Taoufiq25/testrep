214042e037cf6c3f1cf3e846796265620787254c
Taoufiq25 <your_emailexample.com>
2025-09-23 01:33:34 +0200

    actuall

--- show_res_simu.m
+++ show_res_simu.m
@@ -3,18 +3,32 @@ function show_res_simu(ref_signal_name, respo_signal_name)
 
     % logsout muss im base workspace stehen (Signal logging aktivieren)
     %try
-    %    out = evalin('base','out');
-     %   logsout = out.logsout;
+       % out = evalin('base','out');
+        %logsout = out.logsout;
+    persistent runCount
+    if isempty(runCount)
+        runCount = 1;
+        fprintf('runCount %d',runCount);
+    else
+        runCount = runCount + 1;
+        fprintf('runCount %d',runCount);
+    end
 
+    % Skip the very first run
+    if runCount == 1
+        disp('First simulation stopped – skipping results.');
+        return;
+    end
+    
     %catch
      %   disp('Fehler: Variable ''logsout'' nicht im Base Workspace gefunden.');
       %  return
     %end
-
+    
     % Namen anpassen falls deine Signale anders heißen:
     %ref = logsout.getElement('ref').Values;
     %sig = logsout.getElement('speed').Values;
-
+    %assignin('base', 'out', out);
     results = analyze_transitions(ref_signal_name, respo_signal_name);
     if isempty(results)
         disp('Keine Transitionen erkannt.');
